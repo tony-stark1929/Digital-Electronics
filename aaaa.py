3a94b2ad8bb647b47a7f0e0502aecf7e6aa147a1
tony-stark1929 <ayushchauhan1929@gmail.com>
2024-12-24 13:13:38 +0530

    Update aaaa.py

--- aaaa.py
+++ aaaa.py
@@ -44,6 +44,34 @@ for line in f:
     d[l[1]]=d.get(l[1],[])+[int(l[2])]
     d[l[3]]=d.get(l[3],[])+[int(l[4])]
 print(d)
+##################################################
+f=open('scores.txt', 'r')
+games=[] 
+for line in f:
+    L=line.split()
+    games.append([L[1],int(L[2]),L[3],int(L[4])]) 
+# print(games)
+d={}    
+for team1,score1,team2,score2 in games:
+    if team1 not in d:
+        d[team1]={'win':0, 'loss':0,'my_score':0,'against_score':0}
+    if team2 not in d:
+        d[team2]={'win':0, 'loss':0,'my_score':0,'against_score':0}
+    d[team1]['my_score']+=score1
+    d[team1]['against_score']+=score2
+    d[team2]['my_score']+=score2
+    d[team2]['against_score']+=score1
+    if score2>score1:
+        d[team2]['win']+=1
+        d[team1]['loss']+=1
+    else:
+        d[team1]['win']+=1
+        d[team2]['loss']+=1
+count=1        
+for i,j in d.items():
+    if (j['win']>j['loss']) and (j['my_score']<j['against_score']):
+        print(count,i)
+        count+=1                    
 for i,j in d.items():
     if  (sum(j)/len(j))>=70:
         print(i,sum(j)/len(j))
