d2bf2865be20d1b606fe9b3b87923a7e32e943ec
tony-stark1929 <ayushchauhan1929@gmail.com>
2024-12-24 12:33:11 +0530

    Update aaaa.py

--- aaaa.py
+++ aaaa.py
@@ -14,3 +14,16 @@ for line in f:
         else:
             loss += 1
 print(won,loss)
+
+# ##################
+# d={}
+# for line in f:
+#     l=line.split()
+#     if (int(l[2]))-int(l[4])>=30:
+#         d[l[3]]=d.get(l[3],0)+1
+#     if (int(l[4])-int(l[2]))>=30:
+#         d[l[1]]=d.get(l[1],0)+1
+# # print(d)
+# k=list(d.items())
+# k.sort(key=lambda x:x[1],reverse=True)
+# print(k[0][0],k[0][1])
